3085052819b9d11971a2f4d651cdfaa344253de5
Pancamaulana <noorpancamaulana@student.telkomuniversity.ac.id>
2022-10-30 20:33:48 +0700

    TP

---
+++ Panca_Home.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<body style="background-color:grey; ">
+    <nav class="navbar-dark bg-secondary ">
+      <ul class="nav justify-content-center">
+
+        <li class="nav-item ">
+            <h6 style="text-align: center;color:antiquewhite">
+          <a class="nav-link active text-dark" href="Panca_Home.php">
+            Home
+          </a>
+            </h6>
+        </li>
+        <li class="nav-item ">
+            <h6 style="text-align: center;color:antiquewhite">
+          <a class="nav-link active text-dark" href="Panca_Booking.php">
+            Booking
+          </a>
+          </h6>
+        </li>
+      </ul>
+    </nav>
+
+    <div class = "container style="text-align: center; padding-top: 0.5cm;">
+
+      <div class="class text-center mb-5 text-primary">
+        <h3 style="text-align: center;color:blue">
+            <b>WELCOME TO EAD RENT
+            </b>
+        </h3>
+        <p class="bg-secondary" style="text-align:center;color:Black">
+        <b>Find your the best deal, here!
+        </b> 
+        </p>
+
+      </div>
+      <div class="container mt-5">
+        <div class="card-deck">
+          <div class="card text-center" style="width: 15rem;">
+            <img src="Rush.jfif" class="card-img-top" style="height: 12rem;">
+
+            <div class="card-body" style="text-align:left">
+              <h5 class="card-title">Toyota Rush
+              </h5>
+              <p>
+                  <b class="card-title" style="color: gray;">
+                    Rp 200.000 /hari
+                  </b>
+              </p>
+
+              <br>
+            </br>
+                <div class="card-text" style="text-align:center"> 
+                <ul class="list-group list-group-flush">
+                    <div class="list-group-item" style="color: blue;"> 
+                        <b>
+                        5 Kursi
+                        </b>
+                    </div>
+                    <li class="list-group-item" style="color: blue;">
+                        <b>
+                        1500 CC
+                        </b>
+                    </li>
+                    <li class="list-group-item" style="color: red;">
+                        <b>
+                        Manual
+                        </b>
+                    </li>
+                    <li class="list-group-item">
+                    </li>
+                </div>
+            </div>
+
+            <div class="card-footer text-center">
+              <a href="Panca_Booking.php?type=Toyota Rush" class="btn btn-primary form-control">
+                  <b>
+                  Book Now
+                  </b>
+              </a>
+              
+            </div>
+            </ul>
+          </div>
+
+          <div class="card text-center" style="width: 15rem;">
+            <img src="Brio.jfif" class="card-img-top" style="height: 12rem;">
+            <div class="card-body" style="text-align:left">
+              
+            <h5 class="card-title"> 
+              Honda Brio
+            </h5>
+              <p class="card-title" style="color: gray;">
+                  <b>
+                  Rp 150.000 /hari
+                  </b>
+              </p> 
+              <br>
+            </br>
+
+                <div class="card-text" style="text-align:center"> 
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item" style="color: blue;">
+                <b>
+                5 Kursi
+                </b>
+                </li>
+                
+                <li class="list-group-item" style="color: blue;">
+                <b>
+                1300 CC
+                </b>
+                </li>
+                  
+                <li class="list-group-item" style="color: red;">
+                <b>
+                Automatic
+                </b>
+                </li>
+                  
+                <li class="list-group-item">
+                </li>
+                  </div>
+            </div>
+            
+            <div class="card-footer text-center">
+              <a href="Panca_Booking.php?type=Honda Brio" class="btn btn-primary form-control">
+                  <b>
+                  Book Now
+                  </b>
+              </a>
+              
+            </div>
+
+          </div>
+            
+            <div class="card text-center" style="width: 15rem;">
+            <img src="Ertiga.jfif" class="card-img-top" style="height: 12rem;">
+            <div class="card-body" style="text-align:left">
+              
+            <h5 class="card-title">
+            Suzuki Ertiga
+            </h5>
+
+            <p class="card-title" style="color: gray;">
+                <b>
+                  Rp 200.000 /hari
+                </b>
+            </p> 
+
+            <br>
+        </br>
+
+            <div class="card-text" style="text-align:center"> 
+            
+            <ul class="list-group list-group-flush">
+            
+            <li class="list-group-item" style="color:blue;">
+            <b>
+                8 Kursi
+            </b>
+            </li>
+
+            <li class="list-group-item" style="color:blue;">
+            <b>
+                1500 CC
+            </b>
+            </li>
+
+            <li class="list-group-item" style="color:red;">
+            <b>
+                Manual
+            </b>
+            </li>
+            
+            
+            <li class="list-group-item">
+            </li>
+            
+            </div>
+          </div>
+
+            <div class="card-footer text-center">
+              <a href="Panca_Booking.php?type=Nissan GrandLivina" class="btn btn-primary form-control">
+                  <b>
+                  Book Now
+                  </b>
+              </a>
+              
+            </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+      <footer style="text-align: center;color:blue">
+      <b>
+        (Panca_1202204283)
+      </b>
+</footer>
+</body>
+</html>
